b6651899ac5be3d4e42769aee2aad01787169363
kamsi19 <47877728+kamsi19@users.noreply.github.com>
2020-03-25 03:18:22 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+new hoverEffect ({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.2,
+    image1: 'Man1.jpg',
+    image2: 'Man2.jpg',
+    speedIn: 2,
+    speedOut: 5,
+    displacementImage: 'stripe1.png',
+})
